358b7ae6ad4cf9816eec2a7974bd167010585e4a
timothy-lew <timothyjblew2000@gmail.com>
2023-12-05 19:47:38 +0800

    Initial Commit

---
+++ InstantMessaging/InstantMessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantMessagingApp.swift
+//  InstantMessaging
+//
+//  Created by Timothy on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstantMessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
